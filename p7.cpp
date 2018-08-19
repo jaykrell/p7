493926286f420afccf283dc618b6ac0d0c1e83f2
Jay Krell <jaykrell@microsoft.com>
2018-08-19 00:25:40 -0700

    loader baby steps

--- p7.cpp
+++ p7.cpp
@@ -14,18 +14,23 @@
 #include <unistd.h>
 typedef short SHORT;
 typedef int INT;
-typedef unsigned char UINT8, UCHAR;
+typedef unsigned char UINT8, UCHAR, BYTE;
 typedef unsigned short UINT16, USHORT, WORD;
 typedef unsigned UINT32, DWORD, UINT;
-typedef unsigned long long UINT64;
+typedef unsigned long long UINT64, ULONGLONG;
+typedef long long INT64, LONGLONG;
 #endif
-typedef unsigned char byte;
+typedef unsigned char uchar, byte;
+typedef unsigned short ushort;
+typedef unsigned uint;
+typedef uint64_t uint64;
 
 struct image_format_invalid_t { };
 struct host_address_t { };
 struct target_address_t { };
 struct read_size_mismatch_t { };
 struct file_too_large_t { };
+struct file_too_small_t { };
 struct file_zero_size_t { };
 
 struct mmap_t
@@ -158,24 +163,87 @@ struct pecoff_file_header_t
 	void unpack(const pecoff_file_header_packed_t& packed)
 	{
 	}
-	char Machine[2];
-	char NumberOfSections[2];
-	char TimeDateStamp[4];
-	char PointerToSymbolTable[4];
-	char NumberOfSymbols[4];
-	char SizeOfOptionalHeader[2];
-	char Characteristics[4];
+	ushort Machine;
+	ushort NumberOfSections;
+	uint TimeDateStamp;
+	uint PointerToSymbolTable;
+	uint NumberOfSymbols;
+	ushort SizeOfOptionalHeader;
+	uint Characteristics;
+};
+
+uint read8 (void **a)
+{
+	uchar* b = (u*)*a;
+	*a = b + 1;
+	return *b;
+}
+
+uint read16le (void **a)
+{
+	uint b = read8(a);
+	return b | (read8(a) << 8);
+}
+
+uint read32le (void **a)
+{
+	uint b = read16le(a);
+	return b | (read16le(a) << 16);
+}
+
+uint64 read64le (void **a)
+{
+	uint b = read32le(a);
+	return b | ((uint64)read32le(a) << 32);
+}
+
+struct pe_section_header_t
+{
+	char Name[8];
+	uint VirtualAddress;
+	uint SizeOfRawData;
+	uint PointerToRawData;
+	uint PointerToRelocations;
+	uint PointerToLinenumbers;
+	uint NumberOfRelocations;
+	uint NumberOfLinenumbers;
+	uint Characteristics;
+
+	void unpack (const void *a)
+	{
+		memcpy(Name, a, 8);
+		a = (char*)a + 8;
+		VirtualAddress = read32le (&a);
+		SizeOfRawData = read32le (&a);
+		PointerToRawData = read32le (&a);
+		PointerToRelocations = read32le (&a);
+		PointerToLinenumbers = read32le (&a);
+		NumberOfRelocations = read16le (&a);
+		NumberOfLinenumbers = read16le (&a);
+		Characteristics = read32le (&a);
+	}
 };
 
 struct pe_file_header_t
 {
-	UINT Machine;
-	UINT NumberOfSections;
-	UINT TimeDateStamp;
-	UINT PointerToSymbolTable;
-	UINT NumberOfSymbols;
-	UINT SizeOfOptionalHeader;
-	UINT Characteristics;
+	uint Machine;
+	uint NumberOfSections;
+	uint TimeDateStamp;
+	uint PointerToSymbolTable;
+	uint NumberOfSymbols;
+	uint SizeOfOptionalHeader;
+	uint Characteristics;
+
+	void unpack (const void *a)
+	{
+		Machine = read16le (&a);
+		NumberOfSections = read16le (&a);
+		TimeDateStamp = read32le (&a);
+		PointerToSymbolTable = read32le (&a);
+		NumberOfSymbols = read32le (&a);
+		SizeOfOptionalHeader = read16le (&a);
+		Characteristics = read16le (&a);
+	}
 };
 
 struct module_t
@@ -187,6 +255,10 @@ struct module_t
 	std::string target_path;
 	std::vector<byte> data;
 	std::vector<module_t*> dependents;
+	pe_file_header_t file_header;
+
+	int machine;
+	std::vector<section_t*> sections;
 
 	void load(std::string host_path)
 	{
@@ -198,6 +270,8 @@ struct module_t
 			throw file_too_large_t();
 		if  (size == 0)
 			throw file_zero_size_t();
+		if  (size <= 64)
+			throw file_too_small_t();
 		data.resize((size_t)size);
 #ifdef _WIN32
 		DWORD r = 0;
@@ -211,6 +285,12 @@ struct module_t
 		if (r != size)
 			throw read_size_mismatch_t();
 		if (data[0] != 'M' || data[1] != 'Z')
+			throw image_format_invalid_t();
+		size_t offset_to_pe = read32le (&data[60]);
+		if ((offset_to_pe + 4) >= data.size())
+			throw image_format_invalid_t();
+		if (data[offset_to_pe] != 'P' || data[offset_to_pe + 1] != 'E'
+			|| data[offset_to_pe + 2] || data[offset_to_pe + 3])
 			throw image_format_invalid_t();
 	}
 };
